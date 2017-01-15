bc1cfed55bd6cce692cbcbd2e0f3aea21f9cde7f
zhongxuqi <zhongxuqi.1991@gmail.com>
2017-01-15 17:37:57 +0800

    update footer

--- front/src/app/components/footer/footer.jsx
+++ front/src/app/components/footer/footer.jsx
@@ -8,12 +8,12 @@ export default class Footer extends React.Component {
             <div className="netstore-footer">
                 <h3 className="col-md-offset-1">{Language.textMap("Contact Me")}</h3>
                 <p>
-                    <div className="col-md-2" style={{textAlign:"right"}}>{Language.textMap("Address")}:</div>
-                    <div className="col-md-10">{this.props.address}</div>
+                    <div className="col-md-2 col-sm-2" style={{textAlign:"right"}}>{Language.textMap("Address")}:</div>
+                    <div className="col-md-10 col-sm-10">{this.props.address}</div>
                 </p>
                 <p>
-                    <div className="col-md-2" style={{textAlign:"right"}}>{Language.textMap("Phone")}:</div>
-                    <div className="col-md-10">{this.props.phone}</div>
+                    <div className="col-md-2 col-sm-2" style={{textAlign:"right"}}>{Language.textMap("Phone")}:</div>
+                    <div className="col-md-10 col-sm-10">{this.props.phone}</div>
                 </p>
             </div>
         )
